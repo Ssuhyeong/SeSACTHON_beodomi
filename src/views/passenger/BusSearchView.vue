569c24ca2f1184a5c3b97d79011fc3f5cbf5c8d7
leecr1215 <leecr1215@naver.com>
2023-06-05 18:43:09 +0900

    feat: UI 완료 음성 검색 및 전체 텍스트 읽어주는 기능 제외

--- src/views/passenger/BusSearchView.vue
+++ src/views/passenger/BusSearchView.vue
@@ -1,27 +1,87 @@
 <template>
-  <div class="bus-search">
+  <div class="bus-search" id="container">
     <h2 class="title">{{ title }}</h2>
-    <div class="circle"></div>
-    <button class="keyboard-btn">키보드 사용하기</button>
-    <button class="talk-btn">음성버튼</button>
+    <button aria-label="음성검색 버튼" class="circle" @click="record"></button>
+    <button aria-label="키보드 사용하기 버튼" class="keyboard-btn">
+      키보드 사용하기
+    </button>
+
+    <div id="contentDiv" :aria-live="ariaLive" :aria-hidden="ariaHidden">
+      {{ pageContent }}
+    </div>
+    <button
+      aria-label="전체 글을 읽어주는 버튼"
+      class="talk-btn"
+      @click="readWholeText"
+    >
+      음성버튼
+    </button>
   </div>
 </template>
 
 <script>
-  import {ref} from 'vue';
+  import {onMounted, ref} from 'vue';
   export default {
     name: 'BusSearchView',
     setup() {
       const title = ref('버스 노선 혹은 정류장 이름을 입력하세요.');
 
+      // 음성 검색
+      function record() {
+        const self = this;
+
+        self.speechRecognition.onresult = function (event) {
+          self.recognizedText =
+            self.recognizedText + ' ' + event.results[0][0].transcript;
+        };
+
+        this.speechRecognition.start();
+      }
+
+      onMounted(() => {
+        var SpeechRecognition = SpeechRecognition;
+        this.speechRecognition = new SpeechRecognition();
+      });
+
+      // 전체 읽어주기 기능
+      const ariaLive = ref('polite');
+      const ariaHidden = ref('');
+      const pageContent = ref('');
+
+      const readWholeText = () => {
+        const contentElement = document.getElementById('container');
+        const content = contentElement.innerText;
+
+        // 읽어주기 전 잠시 숨김처리
+        ariaHidden.value = true;
+
+        // 스크린 리더에게 전체 내용을 읽어달라는 요청
+        ariaLive.value = 'assertive';
+        pageContent.value = content;
+
+        console.log(pageContent.value);
+        // pageContent.value = '';
+
+        // 읽기가 완료된 후 다시 숨김처리 해제
+        setTimeout(() => {
+          ariaLive.value = 'polite';
+          ariaHidden.value = '';
+        }, 100);
+      };
+
       return {
         title,
+        record,
+        ariaLive,
+        ariaHidden,
+        pageContent,
+        readWholeText,
       };
     },
   };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .bus-search {
     text-align: center;
     display: flex;
@@ -43,6 +103,7 @@
       width: 210px;
       height: 210px;
       border-radius: 50%;
+      border: none;
     }
 
     // 키보드 사용 버튼
@@ -66,6 +127,9 @@
       background-color: $gray;
       border-radius: 81px 81px 0 0;
       border: none;
+    }
+
+    .contentDiv {
     }
   }
 </style>
